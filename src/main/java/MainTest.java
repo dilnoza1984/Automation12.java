0af340cb5289928d07e93e5869b24f0e78fced5a
Dilnoza Akhrarova <akhrarovad@gmail.com>
2023-02-25 15:08:17 -0500

    Initial commit

---
+++ src/main/java/MainTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.junit.jupiter.api.*;
+
+public class MainTest {
+    @Test
+    public void test11() throws InterruptedException {
+        Main m = null;
+        m.test1();
+    }
+}
+
+
